c947d5129f6e4dcd06f611b68981042b78b5e0e7
LAPTOP-A76MUA6E\Linh <lequanglinh1412@gmail.com>
2025-03-06 21:38:38 +0700

    update

--- Unordered_map/unordered_map.cpp
+++ Unordered_map/unordered_map.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
-
+#include <vector>
+#include <algorithm> 
 using namespace std;
 
 string str = "geeksforgeeks";
@@ -31,11 +32,30 @@ void frequencyStr(string str)
 
 }
 
+int majorityElement1(vector<int>& nums) {
+    unordered_map<int, int> myMap;
+    for(int i=0; i<nums.size(); i++)
+    {
+        if(myMap.find(nums[i]) == myMap.end())
+        {
+            myMap[nums[i]] = 1;
+        }
+         else myMap[nums[i]]++;
+        if(myMap[nums[i]] > nums.size()/2) return nums[i];
+    }
+    return 0;
+}
+
+int majorityElement(vector<int>& nums) 
+{
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return nums[n/2];
+}
+vector<int> nums = {3,2,3};
 int main()
 {
-    frequencyStr(str);
-    unordered_map<char, int> mm;
-    cout << mm['x'];
+    cout << majorityElement(nums);
 
     return 0;
 }
